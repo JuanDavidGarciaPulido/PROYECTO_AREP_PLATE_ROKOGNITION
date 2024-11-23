3bb91e8f1e1b36d030d39fa0179092e53b5fc4ce
Larfg <luis.giraldo-r@mail.escuelaing.edu.co>
2024-11-23 17:01:24 -0500

    initial

---
+++ src/main/java/edu/eci/arep/carplaterekognition/CarPlateRekognitionApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.arep.carplaterekognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarPlateRekognitionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarPlateRekognitionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/eci/arep/carplaterekognition/CarPlateRekognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.arep.carplaterekognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPlateRekognitionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
